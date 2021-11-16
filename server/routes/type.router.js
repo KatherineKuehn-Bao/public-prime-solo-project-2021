5d251c6540692d008a8ba9b209d4c17d91db5fa2
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-16 11:03:41 -0600

    adding type router for drop downs

---
+++ server/routes/type.router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// **** GET TYPES FOR DROP DOWN 
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "food_type"`;
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(err => {
+        console.log('error: get all types', err);
+    res.sendStatus (500)        
+    });
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+//added type router 
+const typeRouter = require('../routes/type.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +25,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+//add a type router 
+app.use('/api/type', typeRouter);
 
 // Serve static files
 app.use(express.static('build'));
