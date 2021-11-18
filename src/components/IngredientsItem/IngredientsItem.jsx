1d12a25bbc82a24b8eb5bf88be5570989ed6987d
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-18 09:46:47 -0600

    working on EDIT . got ingredient.id correct

--- src/components/IngredientsItem/IngredientsItem.jsx
+++ src/components/IngredientsItem/IngredientsItem.jsx
@@ -1,18 +1,35 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 
 function IngredientsItem() {
 
     // Get ingredients from the store
     const ingredients = useSelector(store => store.ingredients);
-    //intialize dispatch 
+
+
+    // use state for Form Mode & editID 
+    const [formMode, setFormMode] = useState('add');
+    const [editId, setEditId] = useState(null);
+
+    //intialize hook 
     const dispatch = useDispatch();
 
-    const handleDelete = (ingredient) => {
-        dispatch({ type: 'DELETE_INGREDIENT', payload: ingredient})
+    //EDIT Button 
+    const editIngredient = (ingredient) => {
+        setFormMode('edit');
+        setEditId(ingredient.id);
+        console.log('ingredient id' , ingredient.id);
     }
 
-    // console.log('ingredinet', ingredient);
+
+
+    //Complete and functional Delete 'DELETE_INGREDIENT'
+    const handleDelete = (ingredient) => {
+        dispatch({ type: 'DELETE_INGREDIENT', payload: ingredient })
+    }
+
+
 
     return (
         <>
@@ -21,7 +38,10 @@ function IngredientsItem() {
                     <td> {ingredient.food_name} </td>
                     <td> </td>
                     <td> <button> Consume </button></td>
-                    <td> <button> Edit </button></td>
+
+                    <td> <button onClick={() => editIngredient(ingredient)}> Edit </button></td>
+
+                    {/* //Delete Button works and updates to DB */}
                     <td> <button onClick={() => handleDelete(ingredient)}> Delete </button></td>
                 </trow>
 
