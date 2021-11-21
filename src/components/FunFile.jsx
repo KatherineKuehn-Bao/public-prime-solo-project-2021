3013213177f0db4dbf13442872ecd4050a791312
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-21 12:04:35 -0600

    added Add food form

--- src/components/FunFile.jsx
+++ src/components/FunFile.jsx
@@ -2,6 +2,12 @@ import HomeIcon from '@mui/icons-material/Home';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import LogoutIcon from '@mui/icons-material/Logout';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import { styled } from '@mui/material/styles';
+
+
+
 import { Paper } from '@mui/material';
 import { BottomNavigation } from '@mui/material';
 import { BottomNavigationAction } from '@mui/material';
@@ -16,23 +22,33 @@ function FunFile() {
   //set Value & intialize hooks 
   const [navValue, setNavValue] = useState(``);
   const history = useHistory();
-  
 
+//change pages with icons 
   const handleChange = (event, newValue) => {
     console.log(`this is newValue`, newValue);
     setNavValue(newValue);
     if (newValue === `logout`) {
-        console.log(`log out pop up `);
-        setOpen(true);
+      console.log(`log out pop up `);
+      setOpen(true);
     }
     // else if (newValue === `settings`) {
     //     console.log(`settings popup`);
     //     setOpen(true);
-    }
+    // }
     else {
-        history.push(newValue);
+      history.push(newValue);
     }
-}
+  }
+
+//ADD BUTTON 
+  const StyledFab = styled(Fab)({
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  });
 
 
 
@@ -46,24 +62,38 @@ function FunFile() {
         onChange={(event, newValue) => handleChange(event, newValue)
         }
       >
-        <BottomNavigationAction 
-        value="/user"
-        label="Home" 
-        icon={<HomeIcon />} />
+        <BottomNavigationAction
+          value="/user"
+          label="Home"
+          icon={<HomeIcon />} />
 
         <BottomNavigationAction
-        value="/inventory" 
-        label="Inventory" 
-        icon={<ArchiveIcon />} />
+          value="/inventory"
+          label="Inventory"
+          icon={<ArchiveIcon />} />
 
-        <BottomNavigationAction 
-        value="/figures"
-        label="Charts" 
-        icon={<DonutLargeIcon />} />
+<BottomNavigationAction
+          value="/form"
+          label="Add Food"
+          icon={<AddIcon />} />
 
-        <BottomNavigationAction 
-        value="logout"
-        label="Logout" icon={<LogoutIcon />} />
+
+//not clicking 
+        {/* <StyledFab 
+        color="secondary" 
+        aria-label="add"
+        value="/form">
+          <AddIcon />
+        </StyledFab> */}
+
+        <BottomNavigationAction
+          value="/figures"
+          label="Charts"
+          icon={<DonutLargeIcon />} />
+
+        <BottomNavigationAction
+          value="logout"
+          label="Logout" icon={<LogoutIcon />} />
 
       </BottomNavigation>
     </Paper>
