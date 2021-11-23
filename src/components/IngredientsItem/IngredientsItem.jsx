f8176672b9f9c30cc4fe45e1c8b4879a4fa38ebc
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-11-23 08:52:03 -0600

    refactoring code

--- src/components/IngredientsItem/IngredientsItem.jsx
+++ src/components/IngredientsItem/IngredientsItem.jsx
@@ -26,20 +26,13 @@ function IngredientsItem() {
     // Get ingredients from the store
     const ingredients = useSelector(store => store.ingredients);
 
-
-    // use state for Form Mode & editID 
-    // const [formMode, setFormMode] = useState('add');
-    // const [editId, setEditId] = useState(null);
-
     //intialize hook 
     const dispatch = useDispatch();
     const history = useHistory();
 
     //update item in the DB -UPDATE_ITEM
     const updateIngredient = (ingredient) => {
-        //sagas 
         dispatch({ type: 'UPDATE_ITEM', payload: ingredient });
-        // console.log('clicked, edit item', ingredient);
     }
 
     //Complete and functional Delete 'DELETE_INGREDIENT'
@@ -49,117 +42,110 @@ function IngredientsItem() {
 
     // **********CURRENT PROJECT
     const handleEdit = (ingredient) => {
-        dispatch ({type: 'SET_EDIT_ITEM', payload: props})
+        dispatch({ type: 'SET_EDIT_ITEM', payload: ingredient })
         history.push(`/edit/${ingredient.id}`);
     }
 
     //Get the ingredients that are stored for the table 
     let storedIngredients = ingredients.filter(ingredient => {
-        // console.log('ingredient status', ingredient.status);
         return ingredient.status === 'storage'
-
     });
 
-//CASE SWITCH  --- CLASS for Location 
-const locationClass = (ingredient) => {
-    switch (ingredient.location_id){
-        case 1:  
-            return 'fridge'
-        case 2: 
-            return 'freezer'
-        case 3:
-            return 'pantry'
-        case 4:
-            return 'fresh'
-        default: ''
+    //CASE SWITCH  --- CLASS for Location 
+    const locationClass = (ingredient) => {
+        switch (ingredient.location_id) {
+            case 1:
+                return 'fridge'
+            case 2:
+                return 'freezer'
+            case 3:
+                return 'pantry'
+            case 4:
+                return 'fresh'
+            default: ''
+        }
     }
-}
-
-
 
 
 
     // *******RETURN ************ //
-    return (
-        <>
-            <Table>
+    return (<>
+        <Table>
 
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Ingredients</TableCell>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Ingredients</TableCell>
+                    <TableCell> </TableCell>
+                    <TableCell>Eat</TableCell>
+                    <TableCell>Edit</TableCell>
+                    <TableCell>Delete</TableCell>
+                </TableRow>
+            </TableHead>
+
+            <TableBody>
+                {storedIngredients.map(ingredient =>
+
+                    <TableRow
+                        key={ingredient.id}
+                        className={locationClass(ingredient)}
+                    >
+                        <TableCell> {ingredient.food_name} </TableCell>
                         <TableCell> </TableCell>
-                        <TableCell>Eat</TableCell>
-                        <TableCell>Edit</TableCell>
-                        <TableCell>Delete</TableCell>
+
+
+                        {/* Consumed food gets updated on DB */}
+                        <TableCell>
+                            <CheckCircleIcon
+                                className="eat"
+                                variant="contained"
+                                color="success"
+                                onClick={() => updateIngredient(ingredient)}>
+                                <IconButton>
+                                    Eat
+                                    {/* <Edit fontSize="small" /> */}
+                                </IconButton>
+                            </CheckCircleIcon>
+                        </TableCell>
+
+                        {/* //NEED TO ADD LOGIC STILL  */}
+                        {/* Update thrown away foods on Database */}
+                        <TableCell>
+                            <EditIcon
+                                className="edit"
+                                variant="contained"
+                                color="info"
+                                onClick={() => handleEdit(ingredient)}>
+                                {/* // onClick={() => updateIngredient(ingredient)}> */}
+                                <IconButton>
+                                    Edit
+                                    {/* <Edit fontSize="small" /> */}
+                                </IconButton>
+                            </EditIcon>
+                        </TableCell>
+
+
+
+                        {/* //Delete Button works and updates to DB */}
+                        <TableCell>
+                            <BackspaceIcon
+                                className="Delete"
+                                variant="contained"
+                                color="error"
+                                onClick={() => handleDelete(ingredient)}>
+                                <IconButton>
+                                    Delete
+                                </IconButton>
+
+                            </BackspaceIcon>
+                        </TableCell>
                     </TableRow>
-                </TableHead>
 
-                <TableBody>
-                    {storedIngredients.map(ingredient =>
-
-                        <TableRow 
-                            key={ingredient.id}
-                            className={locationClass(ingredient)}
-                        >
-                            <TableCell> {ingredient.food_name} </TableCell>
-                            <TableCell> </TableCell>
-
-
-                            {/* Consumed food gets updated on DB */}
-                            <TableCell>
-                                <CheckCircleIcon
-                                    className="eat"
-                                    variant="contained"
-                                    color="success"
-                                    onClick={() => updateIngredient(ingredient)}>
-                                    <IconButton>
-                                        Eat
-                                        {/* <Edit fontSize="small" /> */}
-                                    </IconButton>
-                                </CheckCircleIcon>
-                            </TableCell>
-
-                            {/* //NEED TO ADD LOGIC STILL  */}
-                            {/* Update thrown away foods on Database */}
-                            <TableCell>
-                                <EditIcon
-                                    className="edit"
-                                    variant="contained"
-                                    color="info"
-                                    onClick={() => handleEdit(ingredient)}>
-                                    {/* // onClick={() => updateIngredient(ingredient)}> */}
-                                    <IconButton>
-                                        Edit
-                                        {/* <Edit fontSize="small" /> */}
-                                    </IconButton>
-                                </EditIcon>
-                             </TableCell>
+                )}
+            </TableBody>
+        </Table>
 
 
 
-                            {/* //Delete Button works and updates to DB */}
-                            <TableCell>
-                                <BackspaceIcon
-                                    className="Delete"
-                                    variant="contained"
-                                    color="error"
-                                    onClick={() => handleDelete(ingredient)}>
-                                    <IconButton>
-                                        Delete
-                                    </IconButton>
-
-                                </BackspaceIcon>
-                            </TableCell>
-                        </TableRow>
-
-                    )}
-                </TableBody>
-            </Table>
-
-
-
-        </>
-    )
-
+    </>)
 }
 export default IngredientsItem;
